d6c884f83e64e66c110de52087ffc57be9e8677e
Terrance Cazy <terrancecazy@MacBook-Pro.lan>
2021-01-14 06:27:55 -0600

    complete step 1

---
+++ Part1/index.js
@@ -0,0 +1,9 @@
+function play() {
+  const playerSpan = document.getElementById('player');
+
+  if (playerSpan.innerText === 'X') {
+    playerSpan.innerText = 'O';
+  } else {
+    playerSpan.innerText = 'X';
+  }
+}
