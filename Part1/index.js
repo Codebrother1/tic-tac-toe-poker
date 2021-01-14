0efb47901b5f6d59244014aece9ce31349cea7ff
Terrance Cazy <terrancecazy@MacBook-Pro.lan>
2021-01-14 06:29:23 -0600

    complete steps 2 and 3

--- Part1/index.js
+++ Part1/index.js
@@ -1,9 +1,12 @@
-function play() {
+function play(clickedId) {
   const playerSpan = document.getElementById('player');
+  const clickedElement = document.getElementById(clickedId);
 
   if (playerSpan.innerText === 'X') {
     playerSpan.innerText = 'O';
+    clickedElement.innerText = 'X';
   } else {
     playerSpan.innerText = 'X';
+    clickedElement.innerText = 'O';
   }
 }
